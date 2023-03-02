23c03a077618602d8dff0247b143088662e8dc4e
0x59d <puro.n@hotmail.com>
2023-03-03 00:29:48 +1300

    add model validation: required fields

--- Data/PersonData.cs
+++ Data/PersonData.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace NishitBidOneDemo.Data;
 
 public class PersonData
 {
+    [Required]
     [DisplayName("First Name")]
     public string FirstName { get; set; }
-
+    
+    [Required]
     [DisplayName("Last Name")]
     public string LastName { get; set; }
 }
