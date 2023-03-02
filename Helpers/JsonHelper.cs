1107e801575ba6011d3be0a0f21be0d82e84a22c
0x59d <puro.n@hotmail.com>
2023-03-03 00:04:47 +1300

    add read/write json to file

---
+++ Helpers/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace NishitBidOneDemo.Helpers;
+
+public static class JsonHelper
+{
+    public static T? ReadFromFile<T>(string file)
+    {
+        return JsonSerializer.Deserialize<T>(File.ReadAllText(file));
+    }
+
+    public static string Serialize<T>(T data)
+    {
+        return JsonSerializer.Serialize(data);
+    }
+
+    public static void WriteToFile<T>(string file, T data)
+    {
+        File.WriteAllText(file, Serialize(data));
+    }
+}
--- Services/PersonService.cs
+++ Services/PersonService.cs
@@ -1,11 +1,18 @@
 ﻿using NishitBidOneDemo.Data;
+using NishitBidOneDemo.Helpers;
 
 namespace NishitBidOneDemo.Services;
 
 public class PersonService : IPersonService
 {
+    const string _personsFile = "persons.json";
+
     public void CreatePerson(PersonData person)
     {
-        throw new NotImplementedException();
+        var persons = JsonHelper.ReadFromFile<List<PersonData>>(_personsFile);
+
+        persons?.Add(person);
+
+        JsonHelper.WriteToFile(_personsFile, persons);
     }
 }
