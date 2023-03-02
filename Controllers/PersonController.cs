1c4724e8832a838fcd268ce5de4d68b109adc990
0x59d <puro.n@hotmail.com>
2023-03-02 23:28:29 +1300

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NishitBidOneDemo.Models;
+using System.Diagnostics;
+
+namespace NishitBidOneDemo.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
